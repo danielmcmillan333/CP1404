01d5e50740cfe156cad7770f3dc916fd11a791bb
danielmcmillan333 <daniel.mcmillan@my.jcu.edu.au>
2018-08-18 18:49:39 +1000

    refactor months variable to number_of_months for better naming practices and refactor the report printing to calculate_total function

--- practicals/prac_04/total_income.py
+++ practicals/prac_04/total_income.py
@@ -8,15 +8,19 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
+    calculate_total(incomes, number_of_months)
+
+
+def calculate_total(incomes, number_of_months):
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
